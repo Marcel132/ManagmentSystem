01aeceaba163a0a8cd76e5f6beaaf7ce44877215
Marcel <marcelpodsiadlo.business@gmail.com>
2024-08-20 16:52:37 +0200

    Create new components

--- Frontend/app/src/app/app.routes.ts
+++ Frontend/app/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { SignupComponent } from './components/auth/signup/signup.component';
 import { MainComponent } from './components/main/main.component';
 import { SettingsComponent } from './components/common/settings/settings.component';
 import { HomeComponent } from './components/common/home/home.component';
+import { ResourcesComponent } from './components/common/resources/resources.component';
+import { CreateResourceComponent } from './components/common/create-resource/create-resource.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'signup', pathMatch: 'full' },
@@ -12,5 +14,7 @@ export const routes: Routes = [
   {path: 'home', component: MainComponent, children: [
     {path: '', component: HomeComponent},
     {path: 'settings', component: SettingsComponent},
+    {path: 'resources', component: ResourcesComponent},
+    {path: 'create-new-resource', component: CreateResourceComponent},
   ]},
 ];
--- Frontend/app/src/app/components/main/main.module.ts
+++ Frontend/app/src/app/components/main/main.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../common/header/header.component';
 import { FooterComponent } from '../common/footer/footer.component';
+import { HomeComponent } from '../common/home/home.component';
+import { ResourcesComponent } from '../common/resources/resources.component';
+import { SettingsComponent } from '../common/settings/settings.component';
+import { CreateResourceComponent } from '../common/create-resource/create-resource.component';
 
 
 
@@ -11,10 +15,18 @@ import { FooterComponent } from '../common/footer/footer.component';
     CommonModule,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    ResourcesComponent,
+    SettingsComponent,
+    CreateResourceComponent
   ], 
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ResourcesComponent,
+    SettingsComponent,
+    CreateResourceComponent
   ]
 })
 export class MainModule { }
